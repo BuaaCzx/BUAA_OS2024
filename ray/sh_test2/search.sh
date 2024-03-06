5b39c67ee7c52390465956eff05b18819b8c167d
蔡志昕 <22371053@buaa.edu.cn>
2024-03-06 20:26:50 +0800

    fix bug

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -2,9 +2,8 @@
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
 touch $3
-tmp='$1'
-echo $tmp
-echo "grep -n $2 $1 > $3"
-grep -n $2 $1 > $3
-echo "awk -F: '{print $tmp}' > $3"
-awk -F: '{print $1}' $1 > $3
+touch tmp
+echo "grep -n $2 $1 > tmp"
+grep -n $2 $1 > tmp
+awk -F: '{print $1}' tmp > $3
+rm tmp

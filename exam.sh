084d5bd15d6fdc273040898021e229c574c94427
蔡志昕 <22371053@buaa.edu.cn>
2024-03-13 19:41:37 +0800

    !

--- exam.sh
+++ exam.sh
@@ -12,7 +12,7 @@ gcc -o ./hello ./code/*.o
 ./hello 2> ./err.txt
 mv ./err.txt ../
 cd ..
-chmod rw-r-xr-x ./err.txt
+chmod +r,+w,-r,-x,+r,-x ./err.txt
 n1=1
 n2=1
 if $# -eq 1
@@ -21,5 +21,6 @@ if $# -eq 2
 	n1=$1
 	n2=$2
 n=$n1+$n2
-sed -n '$np' >&2
+# cat 111
+sed -n '$np' ./err.txt >&2
 

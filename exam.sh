5deb2a8f2e334d69ed47665016f44e89aa85e45c
蔡志昕 <22371053@buaa.edu.cn>
2024-03-13 19:29:50 +0800

    finished

--- exam.sh
+++ exam.sh
@@ -11,4 +11,14 @@ done
 gcc -o ./hello ./code/*.o
 ./hello 2> ./err.txt
 mv ./err.txt ../
-
+cd ..
+chmod rw-r-xr-x ./err.txt
+n1=1
+n2=1
+if $# -eq 1
+	n1=$1
+if $# -eq 2
+	n1=$1
+	n2=$2
+n=$n1+$n2
+sed -n '$np' >&2

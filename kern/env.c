49f6b5f1ae1920b65e7ca4d5ff551fb53a998661
蔡志昕 <22371053@buaa.edu.cn>
2024-04-22 10:17:36 +0000

    3.6

--- kern/env.c
+++ kern/env.c
@@ -335,6 +335,7 @@ static void load_icode(struct Env *e, const void *binary, size_t size) {
 
 	/* Step 3: Set 'e->env_tf.cp0_epc' to 'ehdr->e_entry'. */
 	/* Exercise 3.6: Your code here. */
+	e->env_tf.cp0_epc = ehdr->e_entry;
 
 }
 

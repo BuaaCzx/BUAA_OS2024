b63fdcd4a8f7f3aaecb02ac16496e5ad89218fde
蔡志昕 <22371053@buaa.edu.cn>
2024-04-22 10:40:23 +0000

    change tests

--- tests/lab3_1/init.c
+++ tests/lab3_1/init.c
@@ -4,7 +4,12 @@ void mips_init(u_int argc, char **argv, char **penv, u_int ram_low_size) {
 	mips_vm_init();
 	page_init();
 
+	printk("finished page_init!\n");
+
 	env_init();
+
+	printk("finished env_init!!\n");
+
 	env_check();
 	halt();
 }

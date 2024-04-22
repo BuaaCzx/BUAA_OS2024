82c14f52746bfb1f53b0166e501977f2784cc2e2
蔡志昕 <22371053@buaa.edu.cn>
2024-04-22 10:33:07 +0000

    fixed 3.8 bug

--- kern/env.c
+++ kern/env.c
@@ -477,7 +477,7 @@ void env_run(struct Env *e) {
 	 *    returning to the kernel caller, making 'env_run' a 'noreturn' function as well.
 	 */
 	/* Exercise 3.8: Your code here. (2/2) */
-	env_pop_tf(curenv->env_tf, curenv->asid);
+	env_pop_tf(curenv->env_tf, curenv->env_asid);
 
 }
 

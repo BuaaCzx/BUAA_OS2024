0be63eb8671a8ea51029d2f5648341ab7a7003e7
蔡志昕 <22371053@buaa.edu.cn>
2024-04-22 13:45:24 +0000

    Update file sched.c

--- kern/sched.c
+++ kern/sched.c
@@ -35,16 +35,19 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
-	count--;
 	if (yield || count == 0 || e == NULL || !e->env_status == ENV_RUNNABLE) {
-		if (e != NULL && e->env_status == ENV_RUNNABLE) {
-			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+		if (e != NULL) {
+			TAILQ_REMOVE(&env_sched_list, curenv, env_sched_link);
+			if (e->env_status == ENV_RUNNABLE) {
+				TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+			}
 		}
 		panic(TAILQ_EMPTY(&env_sched_list));
 		curenv = TAILQ_FIRST(&env_sched_list);
 		count = curenv->env_pri;
-		TAILQ_REMOVE(&env_sched_list, curenv, env_sched_link);
-		// curenv = e;
+		curenv = e;
 	}
+	count--;
+	env_run(e);
 
 }

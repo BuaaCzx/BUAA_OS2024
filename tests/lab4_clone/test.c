1a96cf38df70c11217c74587349b51d96ad00892
蔡志昕 <22371053@buaa.edu.cn>
2024-05-22 12:20:40 +0000

    Update file test.c

--- tests/lab4_clone/test.c
+++ tests/lab4_clone/test.c
@@ -30,7 +30,7 @@ void check() {
 int main() {
 	u_int child_stack = 0x7f3fd800;
 	syscall_clone((void *)check, (void *)child_stack);
-	debugf("### cloned!!!\n");
+	// debugf("### cloned!!!\n");
 	strcpy(buf, input);
 	flag = 1;
 	return 0;

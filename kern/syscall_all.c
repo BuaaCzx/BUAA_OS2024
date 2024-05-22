c26febc4677e3f75083d7825f232fe6aec46b123
蔡志昕 <22371053@buaa.edu.cn>
2024-05-22 12:52:40 +0000

    Update file syscall_all.c

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -22,8 +22,8 @@ int sys_clone(void *func, void *child_stack) {
 	e->env_status = ENV_RUNNABLE;
 	TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
 	// printk("sys_cloned, id is %d, env_cnt = %d!\n", e->env_id, p->env_cnt);
-	// e->env_id += p->env_cnt;
-	return e->env_id;
+	e->env_id += p->env_cnt;
+	return e->env_id - p->env_cnt;
 }
 
 

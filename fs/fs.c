7a5eb4f57e3cf8e951f759f5e169fe4b781406c5
蔡志昕 <22371053@buaa.edu.cn>
2024-05-27 13:13:06 +0000

    Update file fs.c

--- fs/fs.c
+++ fs/fs.c
@@ -177,7 +177,7 @@ void unmap_block(u_int blockno) {
 
 	// Step 3: Unmap the virtual address via syscall.
 	/* Exercise 5.7: Your code here. (5/5) */
-	syscall_mem_unmap(0, va);
+	syscall_mem_unmap(0, disk_addr(blockno));
 
 	user_assert(!block_is_mapped(blockno));
 }

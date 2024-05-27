e520d6cf397875acc7b423a2ad3d2c2b49507a5e
蔡志昕 <22371053@buaa.edu.cn>
2024-05-27 08:43:45 +0000

    5.5

--- fs/fs.c
+++ fs/fs.c
@@ -3,6 +3,14 @@
 
 struct Super *super;
 
+/*
+1 struct Super {
+2 u_int s_magic; // Magic number: FS_MAGIC
+3 u_int s_nblocks; // Total number of blocks on disk
+4 struct File s_root; // Root directory node
+5 };
+*/
+
 uint32_t *bitmap;
 
 void file_flush(struct File *);
@@ -186,10 +194,13 @@ void free_block(u_int blockno) {
 	// You can refer to the function 'block_is_free' above.
 	// Step 1: If 'blockno' is invalid (0 or >= the number of blocks in 'super'), return.
 	/* Exercise 5.4: Your code here. (1/2) */
-
+	if (blockno == 0 || block >= super->s_nblocks) {
+		return;
+	}
 	// Step 2: Set the flag bit of 'blockno' in 'bitmap'.
 	// Hint: Use bit operations to update the bitmap, such as b[n / W] |= 1 << (n % W).
 	/* Exercise 5.4: Your code here. (2/2) */
+	bitmap[blockno / 32] |= 1 << (blockno % 32);
 
 }
 

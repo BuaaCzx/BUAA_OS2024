4d8c293d2cdafd76b63f716054d60cb518faced8
蔡志昕 <22371053@buaa.edu.cn>
2024-06-25 07:39:01 +0000

    Update file ipc.c

--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -48,7 +48,7 @@ int sigaction(int signum, const struct sigaction *newact, struct sigaction *olda
 	if (is_illegal_sig(signum)) {
 		return -1;
 	}
-	return syscall_set_sigaction(0, signum, newact, oldact);
+	return syscall_sigaction(0, signum, newact, oldact);
 }
 
 int kill(u_int envid, int sig){

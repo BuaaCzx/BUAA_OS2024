f25d6a94728bb85c4a21091b051233aa9dcb77ba
蔡志昕 <22371053@buaa.edu.cn>
2024-06-25 19:32:57 +0000

    Update file test.c

--- tests/lab6_challenge_sigaction/test.c
+++ tests/lab6_challenge_sigaction/test.c
@@ -1,18 +1,22 @@
 #include <lib.h>
+#include <signal.h>
 
-void sigsegv_handler(int sig) {
-    debugf("capture SIGSEGV signal.\n");
-    exit();
+void sigchld_handler(int sig) {
+    debugf("capture SIGCHLD signal.\n");
+    debugf("shouldn't see this.\n");
 }
 
 int main() {
     struct sigaction sa;
-    sa.sa_handler = sigsegv_handler;
+    sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
-    sigaction(SIGSEGV, &sa, NULL);
-    int* ptr = (int*)0x0;
-    debugf("Accessing invalid memory address...\n");
-    int val = *ptr; 
-    debugf("If you see this on your screen, it means that the signal is not handled correctly.\n");
-    return 0;
+    sigaction(SIGCHLD, &sa, NULL);
+    u_int pid = syscall_getenvid();
+    if (fork() == 0) {
+        debugf("send kill to %x\n", pid);
+        kill(pid, SIGKILL);
+        exit();
+    }
+    while (1);
+   return 0;
 }
